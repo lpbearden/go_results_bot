907444cdde2b4c8017f4c4e786077963325265ea
Lucas Bearden <lpbearden@gmail.com>
2018-10-20 17:33:20 -0400

    initial commit

---
+++ auth.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+)
+
+func GetTwitterClient() *twitter.Client {
+	consumerKey := CONSUMER_KEY
+	consumerSecret := CONSUMER_SECRET
+	accessToken := ACCESS_TOKEN
+	accessSecret := ACCESS_SECRET
+
+	if consumerKey == "" || consumerSecret == "" || accessToken == "" || accessSecret == "" {
+		log.Fatal("Consumer key/secret and Access token/secret required")
+	}
+
+	config := oauth1.NewConfig(consumerKey, consumerSecret)
+	token := oauth1.NewToken(accessToken, accessSecret)
+	// OAuth1 http.Client will automatically authorize Requests
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	// Twitter client
+	client := twitter.NewClient(httpClient)
+	return client
+}
---
+++ tweeter.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	scraper "csgo_scraper"
+	"fmt"
+	"github.com/dghubble/go-twitter/twitter"
+	"log"
+	s "strings"
+	"time"
+)
+
+var client = GetTwitterClient()
+
+func main() {
+	// sentinel value to check against
+	lastMatch := ""
+	duration := time.Duration(60) * time.Second
+
+	for {
+		fmt.Println("-------------- checking for the latest match")
+		// go get the latest match
+		match := scraper.GetMatch()
+
+		// check if the match is in the last match
+		if match.MatchUrl != lastMatch {
+			// double check by looking at the last tweet
+			if !isLastTweet(match) {
+				// set lastMatch to current match and send tweet
+				lastMatch = match.MatchUrl
+				sendTweet(match)
+			}
+		}
+		fmt.Println("-------------- sleeping")
+		time.Sleep(duration)
+	}
+}
+
+func isLastTweet(m scraper.Match) bool {
+	isLast := false
+	lastMatch := ""
+	homeTimelineParams := &twitter.HomeTimelineParams{
+		Count:     1,
+		TweetMode: "extended",
+	}
+	tweets, _, _ := client.Timelines.HomeTimeline(homeTimelineParams)
+
+	if len(m.Maps) > 0 {
+		lastMatch = fmt.Sprintf("%s beat %s %s-%s on %s", m.Winner, m.Loser, m.WinScore, m.LoseScore, s.Join(m.Maps, ", "))
+	} else {
+		lastMatch = fmt.Sprintf("%s beat %s %s-%s on %s", m.Winner, m.Loser, m.WinScore, m.LoseScore, scraper.CsMap[m.MapName])
+	}
+
+	if s.Contains(tweets[0].FullText, lastMatch) {
+		isLast = true
+		log.Print("current match is same as last match")
+	}
+	return isLast
+}
+
+func sendTweet(m scraper.Match) {
+	tweetStr := ""
+	if len(m.Maps) > 0 {
+		tweetStr = fmt.Sprintf("%s beat %s %s-%s on %s \nhttps://hltv.org%s\n#%s", m.Winner, m.Loser, m.WinScore, m.LoseScore, s.Join(m.Maps, ", "), m.MatchUrl, m.Event)
+	} else {
+		tweetStr = fmt.Sprintf("%s beat %s %s-%s on %s \nhttps://hltv.org%s\n#%s", m.Winner, m.Loser, m.WinScore, m.LoseScore, scraper.CsMap[m.MapName], m.MatchUrl, m.Event)
+	}
+
+	fmt.Println(tweetStr)
+	// tweet, _, _ := client.Statuses.Update(tweetStr, nil)
+	// log.Print("Posted Tweet: ", tweet.Text)
+}
