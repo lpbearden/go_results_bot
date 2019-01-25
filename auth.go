ef5dce1598be51a40ada19b98394ad57b87ee49b
Lucas Bearden <lpbearden@gmail.com>
2019-01-24 23:15:57 -0500

    updated auth to use environment variables to support deploying from github

--- auth.go
+++ auth.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
 )
 
 func GetTwitterClient() *twitter.Client {
-	consumerKey := CONSUMER_KEY
-	consumerSecret := CONSUMER_SECRET
-	accessToken := ACCESS_TOKEN
-	accessSecret := ACCESS_SECRET
+	consumerKey := os.Getenv("TWITTER_CONSUMER_KEY")
+	consumerSecret := os.Getenv("TWITTER_CONSUMER_SECRET")
+	accessToken := os.Getenv("TWITTER_ACCESS_TOKEN")
+	accessSecret := os.Getenv("TWITTER_ACCESS_SECRET")
 
 	if consumerKey == "" || consumerSecret == "" || accessToken == "" || accessSecret == "" {
 		log.Fatal("Consumer key/secret and Access token/secret required")
